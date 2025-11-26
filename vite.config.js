e51ee57bce5023508b1a088e5ef9119e39850518
rahulsengupta <rahulsengupta0011@gmailcom>
2025-11-26 09:29:34 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
+      '/api': 'https://athena-ai-1-5pif.onrender.com'
     }
   },
   plugins: [react()]
