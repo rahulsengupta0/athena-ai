9b210a452c4dc006a9253d26b1a907402b43ae75
pragyasharma12mini-netizen <pragyasharma12mini@gmail.com>
2025-12-05 11:45:14 +0530

    gpt instead of r1

--- chroma_backend/query_server.py
+++ chroma_backend/query_server.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI
 import chromadb
 from chromadb.utils import embedding_functions
+import os
+
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="../backend/.env")
 
 app = FastAPI()
+openai_key = os.getenv("OPENAI_API_KEY")
+
+os.environ["CHROMA_OPENAI_API_KEY"] = openai_key
 
 # ✅ Use Chroma's built-in embedding function
-embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
+embedding_function = embedding_functions.OpenAIEmbeddingFunction(
+    api_key=openai_key,
+    model_name="text-embedding-3-small"
 )
 
-# ✅ Connect to existing Chroma DB
+# Connect to existing Chroma DB
 client = chromadb.PersistentClient(path="./chroma_store")
 
-# ✅ Load existing collection (trained earlier)
+# Load existing collection (trained earlier)
 collection = client.get_collection(
     name="athena_help_docs",
     embedding_function=embedding_function
